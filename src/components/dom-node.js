967dea1f6e9faed12c635bb80518b0f9a6132d56
Nico <nicodiazan@gmail.com>
2023-03-24 20:31:14 +0100

    Implement drawing dom nodes

---
+++ src/components/dom-node.js
@@ -0,0 +1,29 @@
+function drawNode(node) {
+    const domNode = document.createElement("div");
+
+    const style = "";
+    style +=
+        "width: 30px; height: 30px; border: 1px solid black; border-radius: 15px";
+
+    domNode.style = style;
+
+    if (typeof node.portalTarget === "number") {
+        domNode.textContent = node.position - node.portalTarget;
+    }
+
+    return domNode;
+}
+
+function drawString(string) {
+    
+    const domString = document.createElement("div"); 
+
+    for (let i = 0; i < string.length; i++) {
+        const node = string[i];
+        domString.appendChild(drawNode(node));        
+    }
+
+    return domString;
+}
+
+export { drawNode, domString };
