ca99d923a79ea5ae27cedab9d01cfdd9e3ce4f0d
Nico <nicodiazan@gmail.com>
2023-03-24 19:03:08 +0100

    Remove some unnecessary boilerplate.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import _ from "lodash";
 import printMe from "./print.js";
 
 function component() {
@@ -6,7 +5,6 @@ function component() {
     const btn = document.createElement("button");
 
     // Lodash, now imported by this script
-    element.innerHTML = _.join(["Hello", "webpack"], " ");
 
     btn.innerHTML = "Click me and check the console";
     btn.onclick = printMe;
